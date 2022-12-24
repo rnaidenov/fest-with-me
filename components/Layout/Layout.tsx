bc31c528f49301bae8dedb9198190e6a7cd21e90
Radoslav Naydenov <radoslav.naydenov@homagames.com>
2022-12-24 09:26:17 +0100

    Change to 50vh

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -5,8 +5,8 @@ export const Layout: Preact.FC<{ children: Preact.PreactNode }> = (
 ) => {
   return (
     <div className="h-screen bg-gradient-to-b from-[#FFFFFF] via-[#F9F8F8] to-[#D4BEBE]">
-      <div className="absolute w-full h-1/2 bg-hero-festival bg-cover filter drop-shadow-xl" />
-      <div className="absolute w-full h-1/2 bg-black opacity-20" />
+      <div className="absolute w-full h-[50vh] bg-hero-festival bg-cover filter drop-shadow-xl" />
+      <div className="absolute w-full h-[50vh] bg-black opacity-20" />
       {children}
     </div>
   );
