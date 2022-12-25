9f45abcb41345749e02fac3052d14f03f5cc20d7
Radoslav Naydenov <radoslav.naydenov@homagames.com>
2022-12-25 04:27:57 +0100

    Fix mobile footer (hopefully)

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -4,7 +4,7 @@ export const Layout: Preact.FC<{ children: Preact.PreactNode }> = (
   { children },
 ) => {
   return (
-    <div className="h-screen bg-gradient-to-b from-[#FFFFFF] via-[#F9F8F8] to-[#D4BEBE]">
+    <div className="relative h-screen bg-gradient-to-b from-[#FFFFFF] via-[#F9F8F8] to-[#D4BEBE]">
       <div className="absolute w-full h-[50vh] bg-hero-festival bg-cover shadow-3xl" />
       <div className="absolute w-full h-[50vh] bg-black opacity-20" />
       {children}
