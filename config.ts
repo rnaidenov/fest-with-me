fba65ed65172f185ecdb836a017beccf57d55ef8
Radoslav Naydenov <radoslav.naydenov@homagames.com>
2023-04-05 12:55:32 +0200

    Deno env variables

--- config.ts
+++ config.ts
@@ -1,13 +1,9 @@
-import { load } from "https://deno.land/std@0.180.0/dotenv/mod.ts";
-
-const configData = await load();
-
 export default {
   KIWI_API_KEY: Deno.env.get("KIWI_API_KEY"),
-  KIWI_API_SEARCH_HOST: configData.KIWI_API_SEARCH_HOST,
-  KIWI_API_LOCATIONS_HOST: configData.KIWI_API_LOCATIONS_HOST,
+  KIWI_API_SEARCH_HOST: Deno.env.get("KIWI_API_SEARCH_HOST"),
+  KIWI_API_LOCATIONS_HOST: Deno.env.get("KIWI_API_LOCATIONS_HOST"),
   RA_HOST: Deno.env.get("RA_HOST"),
   RA_SEARCH_API: Deno.env.get("RA_SEARCH_API"),
-  AIRBNB_API_KEY: configData.AIRBNB_API_KEY,
-  AIRBNB_API_SEARCH_HOST: configData.AIRBNB_API_SEARCH_HOST,
+  AIRBNB_API_KEY: Deno.env.get("AIRBNB_API_KEY"),
+  AIRBNB_API_SEARCH_HOST: Deno.env.get("AIRBNB_API_SEARCH_HOST"),
 };
